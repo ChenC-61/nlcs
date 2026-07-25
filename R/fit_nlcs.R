c827106e7ce53651e439d8522fc4a6bbdbd67c25
ChenC-61 <wzhnky@hotmail.com>
2026-07-25 14:10:11 +0700

    Update fit_nlcs.R

--- R/fit_nlcs.R
+++ R/fit_nlcs.R
@@ -18,6 +18,10 @@ fit_nlcs <- function(hc_z, parallel_iter = 1000L, loading_tolerance = 1e-8) {
   map <- EFAtools::N_FACTORS(x, criteria = "MAP")
   pa_factors <- as.integer(pa$nfact)
   map_factors <- as.integer(map$n_factors[["MAP_TR2"]])
+  message(
+  "MAP suggests number of factors = ",
+  map$n_factors[["MAP_TR2"]]
+)
   if (is.na(pa_factors) || is.na(map_factors)) {
     stop("Factor-retention analysis did not return a usable PA and MAP result.", call. = FALSE)
   }
